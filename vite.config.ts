b6d4422782842b54e943b1b0c3a2b78eda14971a
Elia Cohen <mrc6113@rit.edu>
2026-07-16 23:25:01 -0400

    fix weird css light-dark() bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
+import { Features } from 'lightningcss';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
   base: "/carter-creator-3/",
+  css: {
+    transformer: "lightningcss",
+    lightningcss: {
+      exclude: Features.LightDark,
+    },
+  },
 });
