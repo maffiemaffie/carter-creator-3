74848e101c516d64b37d7d9ef7cd921ce4ad3435
Elia Cohen <mrc6113@rit.edu>
2026-07-18 10:42:14 -0400

    minify css and remove extra files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
-import { Features } from 'lightningcss';
+import { Features } from "lightningcss";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    cssMinify: "lightningcss",
+  },
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
   base: "/carter-creator-3/",
   css: {
